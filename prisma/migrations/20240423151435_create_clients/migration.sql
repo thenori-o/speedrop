2d92bc240c88efb8842f437353a6f1976c4acb19
thenori-o <the_norio@outlook.com>
2024-04-23 14:39:59 -0300

    fix: update delivery fields

--- prisma/migrations/20240423151435_create_clients/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "clients" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "username" TEXT NOT NULL,
-    "password" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "clients_username_key" ON "clients"("username");
---
+++ prisma/migrations/20240423173536_create_tables/migration.sql
@@ -0,0 +1,31 @@
+-- CreateTable
+CREATE TABLE "deliveryman" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "clients" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "deliveries" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "id_client" TEXT NOT NULL,
+    "id_deliveryman" TEXT,
+    "item_name" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "end_at" DATETIME,
+    CONSTRAINT "deliveries_id_client_fkey" FOREIGN KEY ("id_client") REFERENCES "clients" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "deliveries_id_deliveryman_fkey" FOREIGN KEY ("id_deliveryman") REFERENCES "deliveryman" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "deliveryman_username_key" ON "deliveryman"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "clients_username_key" ON "clients"("username");
