6de228fbd9b0eaa372ada61e97c5e1405c6c3f49
thenori-o <the_norio@outlook.com>
2024-04-23 15:07:40 -0300

    feat: create client authentication middleware

---
+++ src/modules/deliveries/useCases/createDelivery/createDeliveryController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { CreateDeliveryUseCase } from "./createDeliveryUseCase";
+
+export class CreateDeliveryController {
+  
+  async handle(request: Request, response: Response) {
+    const { id_client } = request;
+    const { item_name } = request.body;
+
+    const createDeliveryUseCase = new CreateDeliveryUseCase();
+    const result = await createDeliveryUseCase.execute({
+      id_client,
+      item_name
+    });
+
+    return response.json(result);
+  }
+
+}
